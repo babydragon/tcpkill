f61152041385a7c0ce531dd9f06c93bd36928beb
babydragon <lingjie.jinlj@alibaba-inc.com>
2018-09-30 17:59:31 +0800

    发送sync包以断开半开的连接

--- tcpkill.c
+++ tcpkill.c
@@ -17,6 +17,7 @@
 #include <err.h>
 #include <libnet.h>
 #include <pcap.h>
+#include <pthread.h>
 
 #include "pcaputil.h"
 #include "version.h"
@@ -29,11 +30,19 @@ pcap_t  *pd;
 int     Opt_max_kill = 0;
 int     kill_counter = 0;
 
+struct tcp_connection {
+    u_short sport;
+    u_short dport;
+    char *src;
+    char *dst;
+    libnet_t *l;
+};
+
 static void
 usage(void)
 {
 	fprintf(stderr, "Version: " VERSION "\n"
-		"Usage: tcpkill [-i interface] [-m max kills] [-1..9] expression\n");
+		"Usage: tcpkill [-i interface] [-m max kills] [-1..9] -s SRC_IP:PORT -d DST_IP:PORT\n");
 	exit(1);
 }
 
@@ -97,6 +106,58 @@ tcp_kill_cb(u_char *user, const struct pcap_pkthdr *pcap, const u_char *pkt)
         }
 }
 
+static int build_syn(libnet_t *l, u_short sport, u_short dport, char *srchost, char *dsthost)
+{
+    printf("send fake sync package from %s:%d to %s:%d\n", dsthost, dport, srchost, sport);
+    struct in_addr ip_src, ip_dst;
+    ip_src.s_addr = inet_addr(srchost);
+    ip_dst.s_addr = inet_addr(dsthost);
+    u_int32_t seq = 12345;
+    libnet_clear_packet(l);
+    libnet_build_tcp(dport, sport,
+            seq, 0, TH_SYN, 0, 0, 0, LIBNET_TCP_H,
+            NULL, 0, l, 0);
+    libnet_build_ipv4(LIBNET_IPV4_H + LIBNET_TCP_H, 0,
+            libnet_get_prand(LIBNET_PRu16), 0, 64,
+            IPPROTO_TCP, 0, ip_dst.s_addr,
+            ip_src.s_addr, NULL, 0, l, 0);
+
+    if (libnet_write(l) < 0) {
+        printf("send failed.\n");
+        warn("write");
+    } else {
+        printf("send succ\n");
+    }
+    return 0;
+}
+
+void * trigger(void *data)
+{
+    struct tcp_connection *d = (struct tcp_connection*)data;
+    int count = 5;
+    while(count --) {
+        sleep(1);
+        build_syn(d->l, d->sport, d->dport, d->src, d->dst);
+    }
+    return NULL;
+}
+
+static int split(char *in, int *port, char **host) {
+    int i = 0;
+    while(in[i] && in[i]!=':') {
+        i++;
+    }
+    if (in[i] == 0) {
+        warn("error in addr.");
+        exit(1);
+    }
+    in[i] = 0;
+    *port = atoi(in + i + 1);
+    *host = in;
+    return 0;
+}
+
+
 int
 main(int argc, char *argv[])
 {
@@ -105,11 +166,13 @@ main(int argc, char *argv[])
 	int c;
 	char *p, *intf, *filter, ebuf[PCAP_ERRBUF_SIZE];
 	char libnet_ebuf[LIBNET_ERRBUF_SIZE];
-	libnet_t *l;
+	libnet_t *l, *l_sync;
+	int sport, dport;
+	char *src = NULL, *dst = NULL;
 	
 	intf = NULL;
 	
-	while ((c = getopt(argc, argv, "i:m:123456789h?V")) != -1) {
+	while ((c = getopt(argc, argv, "i:m:s:d:123456789h?V")) != -1) {
 		switch (c) {
 		case 'i':
 			intf = optarg;
@@ -125,6 +188,12 @@ main(int argc, char *argv[])
 			else
 				Opt_severity = atoi(argv[optind] + 1);
 			break;
+		case 's':
+			split(optarg, &sport, &src);
+			break;
+		case 'd':
+			split(optarg, &dport, &dst);
+			break;
 		default:
 			usage();
 			break;
@@ -132,14 +201,10 @@ main(int argc, char *argv[])
 	}
 	if (intf == NULL && (intf = pcap_lookupdev(ebuf)) == NULL)
 		errx(1, "%s", ebuf);
-
-	argc -= optind;
-	argv += optind;
 	
-	if (argc == 0)
-		usage();
-	
-	filter = copy_argv(argv);
+	static char f[1024];
+	sprintf(f, "src port %d and dst port %d and src host %s and dst host %s", sport, dport, src, dst);
+	filter = f;
 	
 	if ((pd = pcap_init(intf, filter, 64)) == NULL)
 		errx(1, "couldn't initialize sniffing");
@@ -149,11 +214,23 @@ main(int argc, char *argv[])
 	
 	if ((l = libnet_init(LIBNET_RAW4, intf, libnet_ebuf)) == NULL)
 		errx(1, "couldn't initialize sending");
+
+	if ((l_sync = libnet_init(LIBNET_RAW4, intf, libnet_ebuf)) == NULL)
+		errx(1, "couldn't initialize sending");
 	
 	libnet_seed_prand(l);
 	
 	warnx("listening on %s [%s]", intf, filter);
-	
+
+	// start new thread to send sync package
+	struct tcp_connection data;
+	data.l = l_sync;
+	data.sport = sport;
+	data.dport = dport;
+	data.src = src;
+	data.dst = dst;
+	pthread_t tid;
+	pthread_create(&tid, NULL, trigger, &data);
 	pcap_loop(pd, -1, tcp_kill_cb, (u_char *)l);
   
 	/* NOTREACHED */
